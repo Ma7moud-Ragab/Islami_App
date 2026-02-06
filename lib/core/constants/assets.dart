bf8b6781f6c6f446b714edec127e3024311a4442
Ma7moud Ragab <eng.m.r.2050@gmail.com>
2026-02-07 00:32:51 +0200

    Initial Islami project setup

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,44 @@
+class AppAssets {
+  // ===== Common =====
+  static const String logo = 'assets/images/common/logo.png';
+  static const String header = 'assets/images/common/header.png';
+  static const String mostRecentBg = 'assets/images/common/most_recent_bg.png';
+
+  // ===== Icons =====
+  static const String icBook = 'assets/images/icons/ic_book.png';
+  static const String icHadith = 'assets/images/icons/ic_hadith.png';
+  static const String icRadio = 'assets/images/icons/ic_radio.png';
+  static const String icSebha = 'assets/images/icons/ic_sebha.png';
+  static const String icTime = 'assets/images/icons/ic_time.png';
+
+  // ===== Onboarding =====
+  static const String onboardingLogo =
+      'assets/images/onboarding/logo_onboarding.png';
+  static const String onboarding1 = 'assets/images/onboarding/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding/onboarding3.png';
+  static const String onboarding4 = 'assets/images/onboarding/onboarding4.png';
+  static const String onboarding5 = 'assets/images/onboarding/onboarding5.png';
+
+  // ===== Quran =====
+  static const String quranBg = 'assets/images/quran/bg_quran.png';
+  static const String quranDetailsBg =
+      'assets/images/quran/bg_quran_details.png';
+  static const String suraNumberFrame =
+      'assets/images/quran/sura_number_frame.png';
+
+  // ===== Hadith =====
+  static const String hadithBg = 'assets/images/hadith/bg_hadith.png';
+  static const String hadithCardBg = 'assets/images/hadith/hadith_card_bg.png';
+
+  // ===== Sebha =====
+  static const String sebhaBg = 'assets/images/sebha/bg_sebha.png';
+  static const String sebhaHeader = 'assets/images/sebha/sebha_header.png';
+  static const String sebhaBody = 'assets/images/sebha/sebha_body.png';
+
+  // ===== Radio =====
+  static const String radioBg = 'assets/images/radio/bg_radio.png';
+  static const String radioOff = 'assets/images/radio/radio_off.png';
+  static const String radioOn = 'assets/images/radio/radio_on.png';
+
+}
